b4c560b95c895e4876445f5fae6c67a57dc911ee
wangchangjiu <wangchangjiu@f3e75b62-d1b0-8e42-8cdc-e2012076c291>
2017-09-23 00:53:00 +0000

    定义一个常量类
    git-svn-id: https://develop.isd3.com.cn/svn/TJA/Build/trunk/tja-root@46 f3e75b62-d1b0-8e42-8cdc-e2012076c291

---
+++ tja-yield-inter/src/main/java/com/df/tja/constant/Constants.java
@@ -0,0 +1,32 @@
+/**
+ * 项目名称:tja-yield-inter
+ *
+ * com.df.tja.constant
+ *
+ * Constants.java
+ * 
+ * 2017年9月23日-上午8:55:18
+ *
+ * 2017 上海一勤-版权所有 
+ */
+
+package com.df.tja.constant;
+
+/**
+ * <p>Constants</p>
+ * 
+ * <p>描述：</p>
+ *
+ * <p>备注：</p>
+ * 
+ * <p>2017年9月23日 上午8:55:18</p>
+ *
+ * @author wang.changjiu
+ * 
+ * @version 1.0.0
+ * 
+ */
+
+public abstract class Constants {
+
+}
